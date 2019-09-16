c8c07a43fae55afade7f88a7f825b122ca4d067c
Alex Garin <lxg1992@gmail.com>
2019-09-16 17:18:08 +0000

    Assert Equal Finished

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("Assert succeeded: " + actual + " = " + expected);
+  } else {
+    console.log("Assert failed: " + actual + " != " + expected);
+  }
+};
+
+assertEqual("Lol","Lol");
+assertEqual(5,4);
+assertEqual(3,3);
+assertEqual("lmao","LMAO");
